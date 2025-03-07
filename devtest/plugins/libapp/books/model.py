25411b516132e8ea26714e6d89cf865e54566307
Claudio Driussi <claudio.driussi@gmail.com>
2025-03-07 12:17:57 +0100

    Revert "added multiple inheritance from plugin source code"
    This reverts commit 97fb8ec2ccf90b746668133072e6ea349158e491.

--- devtest/plugins/libapp/books/model.py
+++ devtest/plugins/libapp/books/model.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Index
-from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
 
 
@@ -16,28 +15,3 @@ class Author:
 
     def __repr__(self):
         return f"<Author {self.full_name}>"
-
-
-class Book:
-
-    @validates('isbn')
-    def validate_isbn(self, key, value):
-        """Validazione ISBN"""
-        if not (len(value) == 10 or len(value) == 13):
-            raise ValueError("ISBN deve essere di 10 o 13 caratteri")
-        return value
-
-    @hybrid_property
-    def is_available(self):
-        """Verifica se il libro è disponibile per il prestito"""
-        return self.status == 'A'
-
-    @hybrid_property
-    def average_rating(self):
-        """Calcola il rating medio del libro"""
-        if not self.reviews:
-            return 0.0
-        return sum(review.rating for review in self.reviews) / len(self.reviews)
-
-    def __repr__(self):
-        return f"<Book {self.title}>"
